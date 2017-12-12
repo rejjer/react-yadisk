274775b23ddc2416c29fc8d1ea52913f05d06852
Ivan Podlevskiy <rejjer@ya.ru>
2017-12-12 17:38:06 +0500

    github-pages config

--- dist/js/app.js
+++ dist/js/app.js
@@ -8396,7 +8396,7 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 var config = exports.config = {
-    appId: '9a024882d06443ca8dd703f599bb138b',
+    appId: '390f5962f4ef4a699474245b229912e9',
     oAuthUrl: 'https://oauth.yandex.ru/authorize?response_type=token&client_id=',
     apiUrl: 'https://cloud-api.yandex.net/v1/disk/'
 };
--- src/app/config.js
+++ src/app/config.js
@@ -1,5 +1,5 @@
 export const config = {
-    appId: '9a024882d06443ca8dd703f599bb138b',
+    appId: '390f5962f4ef4a699474245b229912e9',
     oAuthUrl: 'https://oauth.yandex.ru/authorize?response_type=token&client_id=',
     apiUrl: 'https://cloud-api.yandex.net/v1/disk/'
 }
